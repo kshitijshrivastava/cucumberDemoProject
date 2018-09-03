e3966c569c081da5cd6d287325cf11829d6e99e1
kshitij.shrivastava <kshitij.shrivastava@accenture.com>
2018-09-03 08:24:30 +0530

    Cucumber Demo Project

---
+++ src/main/java/cucumberDemo/CucumberDemoProject/App.java
@@ -0,0 +1,13 @@
+package cucumberDemo.CucumberDemoProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/cucumberDemo/CucumberDemoProject/AttitudeTest.java
@@ -0,0 +1,23 @@
+package cucumberDemo.CucumberDemoProject;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class AttitudeTest {
+
+	
+		
+		@When("I meet him")
+		public void Imeet(String str)
+		{
+			
+		}
+		
+		@Then("hhh")
+		public void thenMethod()
+		{
+			
+		}
+		
+
+	
+}
